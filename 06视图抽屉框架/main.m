39b7ab7e68e5197928ee753b83d1ab11dd22b45a
Dong <haodeping@Show-time.local>
2014-06-12 17:44:51 +0800

    Initial Commit

---
+++ 06视图抽屉框架/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  06视图抽屉框架
+//
+//  Created by Dong on 14-6-12.
+//  Copyright (c) 2014年 itbast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ZDAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ZDAppDelegate class]));
+    }
+}
